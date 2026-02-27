1125d1d27b28708807297699bde7fbdc3a329aa1
Chan Miabek <chanmiabek22@gmail.com>
2026-02-27 15:39:20 +0300

    initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
-const rawBaseUrl = process.env.REACT_APP_API_BASE_URL || "";
+const rawBaseUrl = process.env.REACT_APP_API_BASE_URL || "https://eutrbackendapi.onrender.com";
 
 export const getApiUrl = (path) => {
+
   const normalizedPath = path.startsWith("/") ? path : `/${path}`;
   if (!rawBaseUrl) return normalizedPath;
   return `${rawBaseUrl.replace(/\/$/, "")}${normalizedPath}`;
